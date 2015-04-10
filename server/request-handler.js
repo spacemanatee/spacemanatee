007cb5715f2ee76495b4abf0b402506fdf692966
Seven Li <sevenlist0110@gmail.com>
2015-04-10 10:08:41 -0700

    implement API GET call to yelp

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,30 +1,56 @@
-//var yelpKey = require('./api/api_key');
 var request = require('request');
 var yelp = require('./yelp');
+var key = require('./api/api_key');
 
 
 var yelpClient = yelp.createClient({
-  consumer_key: yelpKey.consumer_key,
-  consumer_secret: yelpKey.consumer_secret,
-  token: yelpKey.token,
-  token_secret:  yelpKey.token_secret,
-  ssl: true
+  consumer_key: key.consumer_key,
+  consumer_secret: key.consumer_secret,
+  token: key.token,
+  token_secret:  key.token_secret,
+  ssl: key.ssl
 });
 
 
 
+var locationArray=[];
 
-exports.requestYelp = function(req, res) {
-  console.log('in side request handler');
-  return res.end();
-  /*
-  yelpClient.search({term: "food", limit: 3, ll: "37.788022,-122.399797"}, function(error, data) {
-  console.log(error);
-  console.log(data);
-  res.send(data);
-  res.end();
+
+var searchYelp = function (req, res, yelpProperty) {
+
+  yelpClient.search({term: "food", limit: 10, ll: "37.788022,-122.399797", sort: 2}, function(error, data) {
+    //console.log(error);
+    for (var i = 0; i < data['businesses'].length; i++) {
+      console.log(data['businesses'][i]['name']);
+    }
+    res.end(JSON.stringify(data));
   }); 
-*/
+
 }
 
+var performSearch = function(req, res) {
+  console.log('in side request handler');
+  
+  // first call google map api to get the longitude and latitude arrays along the path
+  // store the path (longitude and latitude) in array (locationArray);
+
+  // 
+  searchYelp(req, res, null);
+}
+
+/* yelpProperty = {
+  term: "food",
+  limit: 10,
+  ll: ,
+  sort: 2,
+  ....
+}
+*/
+  
+
+
+
+
+
+exports.performSearch = performSearch;
 
--- server/router.js
+++ server/router.js
@@ -1,34 +1,42 @@
 var requestHandler = require('./request-handler');
-var OAuth = require('oauth').OAuth;
 var express = require('express');
 var router = express.Router();
 
-
+/*
 router.use(function timeLog(req, res, next) {
     console.log('Time: ', Date.now());
     next();
 });
+*/
 
 
 console.log('in router!');
 
+
 router.post('/search', function(req, res) {
   console.log('now searching google API and then Yelp');
-  //res.send('now searching google API and then Yelp');
+  // call request-handler to perform the work
+  //requestHandler.performSearch(req, res);
   res.end();
+  
 });
+
+
 
 router.get('/search', function(req, res) {
   console.log('now searching google API and then Yelp');
-  //res.send('now searching google API and then Yelp');
-  res.end('now searching google API and then Yelp');
+  //requestHandler.performSearch(req, res);
+  res.end();
 });
+
+
 
 
 router.post('/*', function(req, res) {
   console.log('invalid address');
   res.redirect('/');
 });
+
 
 router.get('/*', function (req, res) {
   console.log('invalid address');
