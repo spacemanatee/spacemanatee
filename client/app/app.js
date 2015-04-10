da836962cbb61fe4b05e37703482c58147f04f96
travisjn <tdizzletrav@gmail.com>
2015-04-10 16:39:22 -0700

    Add comments

--- client/app/app.js
+++ client/app/app.js
@@ -1,112 +1,69 @@
 angular.module('app', ['autofill-directive', 'ngRoute'])
 
 .controller('mapCtrl', function($scope, Maps) {
-
   $scope.submit = function(city) {
-
     console.log("SCOPE ENTIRE: ", $scope.location);
     var startGeo, endGeo;
-
-    // function codeAddress() {
-    //   var geocoder = new google.maps.Geocoder();
-
-    //   geocoder.geocode( { 'address': $scope.location.start}, function(results, status) {
-    //     if (status == google.maps.GeocoderStatus.OK) {
-    //       map.setCenter(results[0].geometry.location);
-    //       var marker = new google.maps.Marker({
-    //           map: map,
-    //           position: results[0].geometry.location,
-    //           animation: google.maps.Animation.DROP
-    //       });
-
-    //       console.log("RESULTS: ", results, results[0].geometry.location)
-    //       startGeo = results[0].geometry.location.k + "," + results[0].geometry.location.D;
-
-    //       geocoder.geocode( { 'address': $scope.location.end}, function(results, status) {
-    //         if (status == google.maps.GeocoderStatus.OK) {
-
-    //           var marker = new google.maps.Marker({
-    //               map: map,
-    //               position: results[0].geometry.location,
-    //               animation: google.maps.Animation.DROP
-    //           });
-
-    //           endGeo = results[0].geometry.location.k + "," + results[0].geometry.location.D;
-
-    //         } else {
-    //           console.log("Geocode was not successful: " + status);
-    //         }
-
-    //       });
-
-    //     } else {
-    //       console.log("Geocode was not successful: " + status);
-    //     }
-    //   });
-
-    // }
-
-    // codeAddress();
 
     calcRoute();
 
     function calcRoute() {
-
+      //New directionsService object to interact with google maps API
       var directionsService = new google.maps.DirectionsService();
 
       var request = {
-          origin: $scope.location.start,
-          destination: $scope.location.end,
-          travelMode: google.maps.TravelMode.DRIVING
+        //origin and destination are obtained from form on index.html
+        origin: $scope.location.start,
+        destination: $scope.location.end,
+        travelMode: google.maps.TravelMode.DRIVING
       };
 
+      //send request to Google Maps Directions API with request object as data
       directionsService.route(request, function(response, status) {
         if (status == google.maps.DirectionsStatus.OK) {
+          //Update the map on index.html
           directionsDisplay.setDirections(response);
 
           console.log("DIRECTIONS RESPONSE: ", response);
           console.log("LENGTH: ", response.routes[0].overview_path.length);
           console.log(response.routes[0].overview_path);
 
+          //Create random markers to place on map for mockup
           for (var i = 0; i < 20; i++) {
-
             var random = Math.floor(Math.random() * response.routes[0].overview_path.length);
-
             var marker = new google.maps.Marker({
                map: map,
                position: response.routes[0].overview_path[random],
                animation: google.maps.Animation.DROP
             });
-
           }
 
           var waypoints = {};
-
+          //Gather all points along route returned by Google in overview_path property
+          //and insert them into waypoints object to send to server
           for (var j = 0; j < response.routes[0].overview_path.length; j++) {
             waypoints[j] = response.routes[0].overview_path[j].k + "," + response.routes[0].overview_path[j].D;
           }
 
           console.log("WAYPOINTS: ", waypoints);
+          //Send all waypoints along route to server
           Maps.sendPost(waypoints);
 
         } else {
+          //Log the status code on error
           console.log("Geocode was not successful: " + status);
         }
       });
     }
-
   };
-
 })
-
 .factory('Maps', function($http) {
 
-  var getDirections = function (target) {
+  var getDirections = function (target) {};
 
-  };
-
+  //This function sends a POST to the server at route /search with all waypoints along route as data
   var sendPost = function(routeObject){
-    console.log('In sendPost');
+
     $http.post('/search', routeObject)
       .then(function(response, error){
         //POST request successfully sent and response code was returned
