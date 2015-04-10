0c918d7ce3c2acfc9863ebe88ca9891ab40231b2
Paul Brady <pablo.brady@gmail.com>
2015-04-10 16:44:09 -0700

    [Doc] Remove unneccessary comments / spaces.

--- server/router.js
+++ server/router.js
@@ -2,47 +2,27 @@ var requestHandler = require('./request-handler');
 var express = require('express');
 var router = express.Router();
 
-/*
-router.use(function timeLog(req, res, next) {
-    console.log('Time: ', Date.now());
-    next();
-});
-*/
-
-
-console.log('in router!');
-
-
 router.post('/search', function(req, res) {
-  console.log('now searching google API and then Yelp');
+  console.log('(POST "/search") Now searching the Yelp API...');
   // call request-handler to perform the work
   requestHandler.performSearch(req, res);
   //res.end();
-  
 });
-
-
 
 router.get('/search', function(req, res) {
-  console.log('now searching google API and then Yelp');
-  requestHandler.performSearch(req, res);
-  //res.end();
+  console.log('GET "/search" unknown - redirecting to homepage.');
+  res.redirect('/');
 });
-
-
-
 
 router.post('/*', function(req, res) {
-  console.log('invalid address');
+  console.log('POST to unknown page - redirecting to homepage.');
   res.redirect('/');
 });
-
 
 router.get('/*', function (req, res) {
-  console.log('invalid address');
+  console.log('GET to unknown page - redirecting to homepage.');
   res.redirect('/');
 });
-
 
 module.exports = router;
 
