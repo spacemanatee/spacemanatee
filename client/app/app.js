9d15547ad929538f01ad8c8cd8cae678bbcf1caa
David Tan <davidtansf@gmail.com>
2015-04-10 13:41:47 -0700

    add random markers enroute for demo

--- client/app/app.js
+++ client/app/app.js
@@ -7,51 +7,90 @@ angular.module('app', ['autofill-directive', 'ngRoute'])
     console.log("SCOPE ENTIRE: ", $scope.location);
     var startGeo, endGeo;
 
-    function codeAddress() {
-      var geocoder = new google.maps.Geocoder();
+    // function codeAddress() {
+    //   var geocoder = new google.maps.Geocoder();
 
-      geocoder.geocode( { 'address': $scope.location.start}, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-          map.setCenter(results[0].geometry.location);
-          var marker = new google.maps.Marker({
-              map: map,
-              position: results[0].geometry.location,
-              animation: google.maps.Animation.DROP
-          });
-          console.log("RESULTS: ", results, results[0].geometry.location)
-          startGeo = results[0].geometry.location.k + "," + results[0].geometry.location.D;
+    //   geocoder.geocode( { 'address': $scope.location.start}, function(results, status) {
+    //     if (status == google.maps.GeocoderStatus.OK) {
+    //       map.setCenter(results[0].geometry.location);
+    //       var marker = new google.maps.Marker({
+    //           map: map,
+    //           position: results[0].geometry.location,
+    //           animation: google.maps.Animation.DROP
+    //       });
+
+    //       console.log("RESULTS: ", results, results[0].geometry.location)
+    //       startGeo = results[0].geometry.location.k + "," + results[0].geometry.location.D;
+
+    //       geocoder.geocode( { 'address': $scope.location.end}, function(results, status) {
+    //         if (status == google.maps.GeocoderStatus.OK) {
+
+    //           var marker = new google.maps.Marker({
+    //               map: map,
+    //               position: results[0].geometry.location,
+    //               animation: google.maps.Animation.DROP
+    //           });
+
+    //           endGeo = results[0].geometry.location.k + "," + results[0].geometry.location.D;
+
+    //         } else {
+    //           console.log("Geocode was not successful: " + status);
+    //         }
+
+    //       });
+
+    //     } else {
+    //       console.log("Geocode was not successful: " + status);
+    //     }
+    //   });
+
+    // }
+
+    // codeAddress();
+
+    calcRoute();
+
+    function calcRoute() {
+
+      var directionsService = new google.maps.DirectionsService();
+
+      var request = {
+          origin: $scope.location.start,
+          destination: $scope.location.end,
+          travelMode: google.maps.TravelMode.DRIVING
+      };
+
+      directionsService.route(request, function(response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+          directionsDisplay.setDirections(response);
+
+          console.log("DIRECTIONS RESPONSE: ", response);
+          console.log("LENGTH: ", response.routes[0].overview_path.length);
+          console.log(response.routes[0].overview_path);
+
+          for (var i = 0; i < 20; i++) {
+            var random = Math.floor(Math.random() * response.routes[0].overview_path.length);
+            var marker = new google.maps.Marker({
+               map: map,
+               position: response.routes[0].overview_path[random],
+               animation: google.maps.Animation.DROP
+            });
+
+          }
+
+          var locations = {
+            start: startGeo,
+            end: endGeo
+          };
+
+          console.log("LOCATIONS: ", locations);
+          Maps.sendPost(locations);
+
         } else {
           console.log("Geocode was not successful: " + status);
         }
       });
-
-      geocoder.geocode( { 'address': $scope.location.end}, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-
-          var marker = new google.maps.Marker({
-              map: map,
-              position: results[0].geometry.location,
-              animation: google.maps.Animation.DROP
-          });
-
-          endGeo = results[0].geometry.location.k + "," + results[0].geometry.location.D;
-
-        var locations = {
-          start: startGeo,
-          end: endGeo
-        };
-
-        console.log("LOCATIONS: ", locations);
-        Maps.sendPost(locations);
-
-        } else {
-          console.log("Geocode was not successful: " + status);
-        }
-      });
-
     }
-
-    codeAddress();
 
   };
 
