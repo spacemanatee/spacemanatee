0c39b73bd25657199e2452685cd94c68cc339b80
travisjn <tdizzletrav@gmail.com>
2015-04-09 19:40:22 -0700

    Create POST request from Angular

--- client/app/app.js
+++ client/app/app.js
@@ -4,22 +4,34 @@ angular.module('app', [])
 
   $scope.submit = function(city) {
 
-      console.log("SCOPE LOCATION: ", $scope.city);
+      //console.log("SCOPE LOCATION: ", $scope.city);
 
-      Maps.getCity($scope.city);
+      //Maps.getCity($scope.city);
 
+      Maps.sendPost({start: 'street', end: 'otherStreet'});
     }
-  };
+
 
 })
 
-.factory('Maps', function() {
+.factory('Maps', function($http) {
 
   var getDirections = function (target) {
 
   };
 
+  var sendPost = function(routeObject){
+    $http.post('/search', routeObject)
+      .then(function(response, error){
+        //POST request successfully sent and response code was returned
+        console.log('response: ', response);
+        console.log('error: ', error);
+        return response;
+      })
+    };
+
   return {
+    sendPost: sendPost,
     getDirections: getDirections
   };
 
