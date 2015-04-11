4d500d0227ddeb5089518a65bfa8fc44c84f0de0
David Tan <davidtansf@gmail.com>
2015-04-10 20:12:57 -0700

    set delay on pin drops

--- client/app/app.js
+++ client/app/app.js
@@ -32,28 +32,40 @@ angular.module('app', ['autofill-directive', 'ngRoute'])
           console.log("LENGTH: ", response.routes[0].overview_path.length);
           console.log(response.routes[0].overview_path);
 
-          //Create random markers to place on map for mockup
-          for (var i = 0; i < 20; i++) {
-            var random = Math.floor(Math.random() * response.routes[0].overview_path.length);
-            var marker = new google.maps.Marker({
-               map: map,
-               position: response.routes[0].overview_path[random],
-               animation: google.maps.Animation.DROP
-            });
-            attachInstructionText(marker, 'hello, world');
-            markerArray[i] = marker;
+          //create random markers to place on map for mockup
+
+          function placemarkers () {
+            for (var i = 0; i < 20; i++) {
+              setDelay(i);
+            }
+
+            function setDelay(i) {
+              setTimeout(function() {
+                var random = Math.floor(Math.random() * response.routes[0].overview_path.length);
+                var marker = new google.maps.Marker({
+                   map: map,
+                   title: "Hello World!",
+                   position: response.routes[0].overview_path[random],
+                   animation: google.maps.Animation.DROP
+                });
+                attachInstructionText(marker, 'hello, world');
+                markerArray[i] = marker;
+              }, i * 300);
+            }
+
+            var waypoints = {};
+            //gather all points along route returned by Google in overview_path property
+            //and insert them into waypoints object to send to server
+            for (var j = 0; j < response.routes[0].overview_path.length; j++) {
+              waypoints[j] = response.routes[0].overview_path[j].k + "," + response.routes[0].overview_path[j].D;
+            }
+
+            console.log("WAYPOINTS: ", waypoints);
+            //Send all waypoints along route to server
+            Maps.sendPost(waypoints);
           }
 
-          var waypoints = {};
-          //Gather all points along route returned by Google in overview_path property
-          //and insert them into waypoints object to send to server
-          for (var j = 0; j < response.routes[0].overview_path.length; j++) {
-            waypoints[j] = response.routes[0].overview_path[j].k + "," + response.routes[0].overview_path[j].D;
-          }
-
-          console.log("WAYPOINTS: ", waypoints);
-          //Send all waypoints along route to server
-          Maps.sendPost(waypoints);
+          placemarkers();
 
         } else {
           //Log the status code on error
