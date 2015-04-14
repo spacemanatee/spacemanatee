fc489865d0a670daecdc950cf4c2b3f90a3c72cd
travisjn <tdizzletrav@gmail.com>
2015-04-13 17:43:11 -0700

    Improve google coordinates filter

--- server/filters/filterGoogle.js
+++ server/filters/filterGoogle.js
@@ -4,26 +4,45 @@
 //Google also returns total distance value in meters which may be
 //useful in determining how far apart the yelp requests should be
 
-var filter = function(coordObj){
+var filter = function(requestBody){
   //coordArray is the overview_path array from Google
 
-  filteredCoords = [];
+  //The best way to ensure we get every part of the road would be to use the total trip distance
+  //and divide that by the number of coordinates in coordObj.  This would give us the distance
+  //between each coordinate allowing us to determine how many/which coordinates we want to send
+  //to yelp
 
-  //start the loop 10% along the trip so we don't return results
-  //in the area where the trip starts
-  /*
-  for(var i = Math.floor(coordArray.length / 10); i < coordArray.length;i){
-    filteredCoords.push(coordArray[i]);
-    //check yelp every 10% of the journey (for testing purposes)
-    i *= 10;
-  }
-  */
+  var distance = requestBody.distance;
+  var coordObj = requestBody.waypoints;
 
-  var key = 1;
-  while (key in coordObj) {
-    filteredCoords.push(coordObj[key]);
-    key += 20;
+  //The distance between each yelp query in miles (i.e. Yelp will be queried every 10 miles along the route)
+  var distanceBetweenQueries = 10;
+
+  //parse distance into an int
+  distance = distance.split(" ");
+  distance = parseInt(distance[0]);
+
+  //Convert coordObj from an object to an array to calculate distance between points
+  var coordArray = [];
+  for(var key in coordObj){
+    coordArray.push(coordObj[key]);
   }
+
+  var distanceBetweenPoints = distance / coordArray.length;
+
+  var counter = 0;
+  var filteredCoords = [];
+
+  //Loop through array and only push the coordinates that are distanceBetweenQueries apart
+  for (var i = 0; i < coordArray.length; i++){
+    if(counter > distanceBetweenQueries){
+      filteredCoords.push(coordArray[i]);
+      counter = 0;
+    } else {
+      counter += distanceBetweenPoints;
+    }
+  }
+
   return filteredCoords;
 }
 
