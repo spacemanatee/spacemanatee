bb74fafc03dbad7a5a97ab646c8eaa87a7d579d8
Paul Brady <pablo.brady@gmail.com>
2015-04-14 04:33:02 -0700

    Minimal filter functionality. We seem to not be getting an even distribution of locations from Yelp.  But maybe this is simply because of what is available?

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,3 +1,5 @@
+'use strict'
+
 var request = require('request');
 var yelp = require('./yelp');
 var key = require('./api/api_key');
@@ -53,37 +55,67 @@ var searchYelp = function (req, res, googleCoords, callback) {
   }
 };
 
+
+
 //Filter results returned from Yelp into an overall top 10
-var createTopResultsJSON = function(yelpResults) {
-  var topResults = [];
-  var index = 0;
-  var length = yelpProperty.limit || 0;
-  var idealLength = 10;
+var getYelpLocations = function(yelpResults, totalYelpSections) {
 
-  while(index < idealLength && index < length) {
-    var name = yelpResults[0]['businesses'][index]['name'];
-    var address = yelpResults[0]['businesses'][index]['location']['display_address'];
-    var rating = yelpResults[0]['businesses'][index]['rating'];
-    var review_count = yelpResults[0]['businesses'][index]['review_count'];
-
-    //console.log("INDEX " + index, yelpResults[0]['businesses'][index]);
-    //console.log(">>> " + name + ": ", address);
-    //console.log("--> rating: " + rating + "  review_count: " + review_count);
-
-    // STILL NEEDS ADDITIONAL FILTERS, EX:
-
-    // IF ( rating >= 4 && review_count > 50 )
-    // More changes to come...   ~Paul
-    topResults.push(yelpResults[0]['businesses'][index]);
-    index++;
+  // Copy all of this location's results, into tempResults.
+  var sectionResults = [];
+  var maxResults = parseInt(yelpProperty['limit']) || 0;
+  for (var s = 0; s < totalYelpSections; s++) {
+    console.log("TRIP SEGMENT:  " + s);
+    if(!yelpResults[s] || yelpResults[s]===undefined || yelpResults[s]['businesses']===undefined || yelpResults[s]['businesses'].length==0) { 
+      console.log("Skipping:  No valid locations found in road segment " + s);
+    } else {
+      var index = 0;
+      if(yelpResults[s]['businesses'] && yelpResults[s]['businesses'].length>0) {
+        while(index<maxResults) {
+          if(yelpResults[s]['businesses'][index]!==undefined) {
+            console.log(" --> " + yelpResults[s]['businesses'][index]['name'] + ", ", yelpResults[s]['businesses'][index]['location']['display_address']);
+            sectionResults.push(yelpResults[s]['businesses'][index]);
+          }
+          index++;
+        }
+      }
+    }
   }
+  return sectionResults;
+}
+
+var createTopResultsJSON = function(yelpResults) {
+  var allYelpLocations = [];
+  var finalResults = [];
+  var minRating = 4;
+
+  // Collect Yelp results for all trip sections.
+  allYelpLocations = getYelpLocations(yelpResults, 10);
+
+  // Select random points of interest
+  var count = 0;
+  var totalFound = 0;
+  console.log("TOTAL LOCATIONS ==> " + allYelpLocations.length);
+  while(count<allYelpLocations.length && totalFound<10) {
+    var randIndex = Math.floor(Math.random() * (allYelpLocations.length-1));
+    console.log("Random #" + randIndex + " = " + allYelpLocations[randIndex].name + "; " + allYelpLocations[randIndex].location.display_address);
+    var aLocation = allYelpLocations[randIndex];
+    if(aLocation && aLocation['rating'] && parseInt(aLocation['rating'])>=minRating ) {
+      finalResults.push(aLocation);
+      allYelpLocations.splice(count, 1);
+    }
+    totalFound++;
+    count++;
+  }
+  // finalResults = allYelpLocations;
+  console.log("Found " + finalResults.length + " final locations.");
 
   var result = {
-    results: topResults
+    results: finalResults
   };
 
   return result;
-}
+};
+
 
 // function to perform the search
 var performSearch = function(req, res, googleCoords) {
@@ -91,7 +123,6 @@ var performSearch = function(req, res, googleCoords) {
   searchYelp(req, res, googleCoords, function(yelpResults) {
     //The callback will call createTopResultsJSON which filters all the results into an overall top 10
     var topResults = createTopResultsJSON(yelpResults);
-    console.log(topResults);
     //Return overall top 10 data in the body of the web response
     res.end(JSON.stringify(topResults));
   });
