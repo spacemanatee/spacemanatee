ff5d8ee6e8cc80a54eddbfbbe8dc4f351dc473ef
Seven Li <sevenlist0110@gmail.com>
2015-04-16 15:19:03 -0700

    [feature] clean up google filter

--- server/filters/filterGoogle.js
+++ server/filters/filterGoogle.js
@@ -1,31 +1,5 @@
 //Filter coordinates received from front end POST request
 //into a smaller number of coordinates for Yelp requests
-if (typeof(Number.prototype.toRad) === "undefined") {
-  Number.prototype.toRad = function() {
-    return this * Math.PI / 180;
-  }
-}
-
-// calculate the distance between 2 waypoints, given their latitudes and longitudes, return distance in miles
-function calcDistance(pt1, pt2) {
-  var R = 6371; // earth radius, in km
-  var lat1= pt1.location.coordinate['latitude']; 
-  var lon1= pt1.location.coordinate['longitude'];
-  var lat2= pt2.location.coordinate['latitude'];
-  var lon2= pt2.location.coordinate['longitude'];
-
-
-  var dLat = (lat2-lat1).toRad(); 
-  var dLon = (lon2-lon1).toRad();
-  var lat1 = lat1.toRad();
-  var lat2 = lat2.toRad();
-
-  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-          Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-  var distance = R * c * 0.621371; // convert distance from km to miles
-  return distance;
-}
 
 //Google also returns total distance value in meters which may be
 //useful in determining how far apart the yelp requests should be
