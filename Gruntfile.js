34cd92a032138cd80f5b5fe45d2777756636b95a
travisjn <tdizzletrav@gmail.com>
2015-04-20 15:32:58 -0700

    Update gruntfile and package.json to build site when deployed

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,8 @@
 module.exports = function(grunt){
 
   grunt.initConfig({
+    clean: ['client/dist/'],
+
     jshint: {
       files: ['Gruntfile.js', 'client/app/*.js']
     },
@@ -27,14 +29,23 @@ module.exports = function(grunt){
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
   grunt.registerTask('lint', [
     'jshint'
   ]);
 
   grunt.registerTask('default', [
+    'clean',
     'jshint',
     'concat',
     'uglify'
   ]);
+
+  grunt.registerTask('build', [
+    'clean',
+    'concat',
+    'uglify'
+  ])
+
 };
