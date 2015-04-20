16ef7ab2fe4bdfad5953653b6ab86699341bcb60
David Tan <davidtansf@gmail.com>
2015-04-19 18:17:52 -0700

    [cleanup] setup config vars on heroku

--- server/helpers/yelpFunctions.js
+++ server/helpers/yelpFunctions.js
@@ -3,11 +3,11 @@ var key = require('../api/api_key');
 
 // create yelp client using Oauth
 var yelpClient = yelp.createClient({
-  consumer_key: key.consumer_key,
-  consumer_secret: key.consumer_secret,
-  token: key.token,
-  token_secret:  key.token_secret,
-  ssl: key.ssl
+  consumer_key: process.env.KEY || key.consumer_key,
+  consumer_secret: process.env.CONSUMER_SECRET || key.consumer_secret,
+  token: process.env.TOKEN || key.token,
+  token_secret: process.env.TOKEN_SECRET || key.token_secret,
+  ssl: process.env.SSL || key.ssl
 });
 
 // yelp search parameter configuration
