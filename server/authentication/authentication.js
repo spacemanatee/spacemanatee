ad2b5507527386ad831c03da99d149b0e0eb0467
Sean Conroy <conroysean@gmail.com>
2015-04-24 12:18:47 -0700

    [refactor] Cleanup path for google passport callback Url

--- server/authentication/authentication.js
+++ server/authentication/authentication.js
@@ -5,6 +5,8 @@ var passport = require('passport'),
 if (!process.env.GOOGLE_ID) {
   var key = require('../api/api_key').google,
   var callbackCurrentURL = "http://localhost:3456/main/auth/success";
+} else {
+  var callbackCurrentURL = process.env.CALLBACK_URL;
 }
 
 passport.serializeUser(function(user, done) {
@@ -18,7 +20,7 @@ passport.deserializeUser(function(user, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_ID || key.clientID,
     clientSecret: process.env.GOOGLE_SECRET || key.clientSecret,
-    callbackURL: callbackCurrentURL || "http://super-roadtrip-advisor.herokuapp.com/main/auth/success";
+    callbackURL: callbackCurrentURL;
   },
   function(accessToken, refreshToken, profile, done) {
     process.nextTick(function () {
